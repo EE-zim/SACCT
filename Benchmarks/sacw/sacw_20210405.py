6c891a53a70bc7911cbd1de491bae747cca632ea
EEzim <43553973+EEzim-2333@users.noreply.github.com>
2022-09-01 02:03:33 +0100

    File update

--- Benchmarks/sacw/sacw_20210405.py
+++ Benchmarks/sacw/sacw_20210405.py
@@ -33,7 +33,7 @@ parser.add_argument('--seed', type=int, default=123456, metavar='N',
                     help='random seed (default: 123456)')
 parser.add_argument('--batch_size', type=int, default=256, metavar='N',
                     help='batch size (default: 256)')
-parser.add_argument('--num_steps', type=int, default=100001, metavar='N',
+parser.add_argument('--num_steps', type=int, default=100098, metavar='N',
                     help='maximum number of steps (default: 1000000)')
 parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
                     help='hidden size (default: 256)')
